08ecf6cb5011625f6f9444e44c3c492e9d99a5b0
“gooder08” <gooder08@yandex.ru>
2023-04-05 11:50:06 +0300

    select is edited

--- task_4/select.sql
+++ task_4/select.sql
@@ -4,10 +4,10 @@ LEFT JOIN genre_executor ge ON g.id = ge.genre_id
 GROUP BY name;
 
 --Количество треков, вошедших в альбомы 2009–2010 годах. "Тут изменил года на 2009-2010, т к 2019-2020 у меня нет"
-SELECT name_album, count(name_track) FROM album a
-FULL JOIN track t ON a.id = t.album_id 
-WHERE release_date >= '01.01.2009' AND release_date <= '31.12.2010'
-GROUP BY name_album;
+SELECT count(name_track) FROM track t 
+JOIN album a  ON a.id = t.album_id 
+WHERE release_date BETWEEN '01.01.2009' AND '31.12.2010';
+
 
 --Средняя продолжительность треков по каждому альбому
 SELECT (name_album), date_trunc('second', avg(length)) FROM album a 
@@ -15,10 +15,14 @@ FULL JOIN track t  ON t.album_id = a.id
 GROUP BY name_album;
 
 --Все исполнители, которые не выпустили альбомы в 1995 году. "Тут изменил год на 1995, т к 2020 у меня нет"
-SELECT fio, name_album, release_date FROM executor_album ea
-FULL JOIN executor e ON ea.executor_id = e.id 
-FULL JOIN album a ON ea.album_id = a.id
-WHERE release_date < '01.01.1995' OR release_date > '31.12.1995';
+SELECT fio FROM executor e 
+WHERE fio NOT IN (
+	SELECT fio FROM executor e1 
+	JOIN executor_album ea ON e1.id = ea.executor_id 
+	JOIN album a ON ea.album_id = a.id
+	WHERE release_date BETWEEN '01.01.1995' AND '31.12.1995'
+);
+
 
 --Названия сборников, в которых присутствует конкретный исполнитель (Глюкоза)
 SELECT name_collection, fio FROM collection c 
@@ -29,12 +33,14 @@ FULL JOIN executor_album ea ON a.id = ea.album_id
 FULL JOIN executor e ON ea.executor_id = e.id 
 WHERE fio LIKE 'Глюкоза';
 
---Названия альбомов, в которых присутствуют исполнители более чем одного жанра.(К сожалению, у меня таких нет: 1 альбом = 1 исполнитель (выдает пустой список))
-SELECT name_album, fio FROM album a 
+--Названия альбомов, в которых присутствуют исполнители более чем одного жанра.
+SELECT name_album, count(name) FROM album a 
 FULL JOIN executor_album ea ON a.id = ea.album_id
 FULL JOIN executor e ON ea.executor_id = e.id 
-GROUP BY name_album, fio 
-HAVING count(fio)>1;
+FULL JOIN genre_executor ge ON e.id = ge.executor_id 
+FULL JOIN genre g ON ge.genre_id = g.id 
+GROUP BY name_album 
+HAVING count(name)>1;
 
 --Наименования треков, которые не входят в сборники (у меня все треки входят в сборники, поэтому пустой список)
 SELECT name_track, name_collection FROM track t 
